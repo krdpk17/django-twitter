21a9c359408ecdd1fd9a1031094a03e932215bbc
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-09 18:22:30 +0530

    http://127.0.0.1:8000/tweetfetch/login/ will allow to login existing registered user
    After login, user will be redirected to profile page which doesn't exist right now.

--- twittertools/tweetfetch/urls.py
+++ twittertools/tweetfetch/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import include,url
+from django.contrib.auth.views import LoginView
+
 from . import views 
 # from django.views.generic import TemplateView
 
@@ -8,6 +10,8 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
 
     url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
+    url(r'^login/$', LoginView.as_view()),
 
     #/tweetfetch/fetcher/add
     url(r'fetcher/add/$', views.CommandCreate.as_view(), name="fetcher-add"),
