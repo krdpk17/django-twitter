b7e003f57c28a669ac86b13b55a1e115e3abc9b0
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-09 19:58:49 +0530

    user logout via logout button
    Next action is to move login logout to accounts section

--- twittertools/tweetfetch/urls.py
+++ twittertools/tweetfetch/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include,url
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
 from . import views 
 # from django.views.generic import TemplateView
@@ -11,7 +12,9 @@ urlpatterns = [
 
     url(r'^register/$', views.UserFormView.as_view(), name='register'),
 
-    url(r'^login/$', LoginView.as_view()),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+
+    url(r'^logout/$', LogoutView.as_view(next_page=reverse_lazy('tweetfetch:login') ), name = "logout"),
 
     #/tweetfetch/fetcher/add
     url(r'fetcher/add/$', views.CommandCreate.as_view(), name="fetcher-add"),
--- twittertools/twittertools/settings.py
+++ twittertools/twittertools/settings.py
@@ -119,4 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL='/tweetfetch/login/'
 LOGIN_REDIRECT_URL = '/tweetfetch'
