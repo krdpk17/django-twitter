d28ef135acac8870e525878a6979ea585423cce3
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-10 08:20:31 +0530

    removed user login related code fro tweetfetch app

--- twittertools/tweetfetch/urls.py
+++ twittertools/tweetfetch/urls.py
@@ -9,13 +9,6 @@ app_name = 'tweetfetch'
 urlpatterns = [
 	#/tweetfetch/
     url(r'^$', views.IndexView.as_view(), name='index'),
-
-    url(r'^register/$', views.UserFormView.as_view(), name='register'),
-
-    url(r'^login/$', LoginView.as_view(), name='login'),
-
-    url(r'^logout/$', LogoutView.as_view(next_page=reverse_lazy('tweetfetch:login') ), name = "logout"),
-
     #/tweetfetch/fetcher/add
     url(r'fetcher/add/$', views.CommandCreate.as_view(), name="fetcher-add"),
 
--- twittertools/tweetfetch/views.py
+++ twittertools/tweetfetch/views.py
@@ -2,11 +2,9 @@ from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
 from django.views.generic import View
 
 from .models import Fetcher
-from .forms import UserForm
 
 
 class IndexView(generic.ListView):
@@ -18,33 +16,3 @@ class IndexView(generic.ListView):
 class CommandCreate(CreateView):
     model = Fetcher
     fields = ['search_term', 'categories_list', 'tweet_filter']
-
-class UserFormView(View):
-    form_class = UserForm
-    template_name = 'tweetfetch/registration_form.html'
-
-    #Display blank form
-    def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form': form})
-
-    #Process form data
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-
-            # Cleaned (normalized) data
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user.set_password(password)
-            user.save()
-
-            # Returns User objects if credential are correct
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return redirect('tweetfetch:index')
-                    
-        return render(request, self.template_name, {'form': form})
