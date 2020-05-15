f822305ce32b1862e2e1cf1c8c3944751d27c3f6
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-15 14:56:46 +0530

    removed edit_datetime from display

--- twittertools/tweetfetch/tables.py
+++ twittertools/tweetfetch/tables.py
@@ -4,4 +4,4 @@ from .models import Fetcher
 class FetcherTable(tables.Table):
     class Meta:
         model = Fetcher
-        fields = ['id', 'categories_list', 'search_term','edit_datetime', 'state',  'type', 'need_filter', 'retweets_of']
+        fields = ['id', 'categories_list', 'search_term','state',  'type', 'need_filter', 'retweets_of']
